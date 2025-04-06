2f7c6cb09e08f0e53ae8948a68c50c025fadb1c9
João Roblez <joaoroblez76@gmail.com>
2025-04-06 11:47:58 -0300

    Primeiro commit - projeto spring boot iniciado

---
+++ src/main/java/com/TCCfootball/TCC_Football/TccFootballApplication.java
@@ -0,0 +1,13 @@
+package com.TCCfootball.TCC_Football;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccFootballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TCCfootball/TCC_Football/TccFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TCCfootball.TCC_Football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccFootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
